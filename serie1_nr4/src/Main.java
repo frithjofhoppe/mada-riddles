5e02afcc5628fbcb725796a6815dc6a867dda1c0
frithjofhoppe <frithjof.hoppe@gmail.com>
2023-02-22 22:17:25 +0100

    feat: Implemented sieve and creat example for data encryption

--- serie1_nr4/src/Main.java
+++ serie1_nr4/src/Main.java
@@ -1,6 +1,41 @@
+import javax.crypto.Cipher;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        sampleDeEncrypting();
+    }
+
+    public static void sampleDeEncrypting() {
+        KeyPairGenerator keyGen = null;
+        try {
+            keyGen = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        keyGen.initialize(4096);
+        KeyPair keypair = keyGen.genKeyPair();
+        PrivateKey privateKey = keypair.getPrivate();
+        PublicKey publicKey = keypair.getPublic();
+
+        try {
+            Cipher cipher1 = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher1.init(Cipher.ENCRYPT_MODE, publicKey);
+            cipher1.update("My original text".getBytes());
+            byte[] encrytpted = cipher1.doFinal();
+            System.out.println(new String(encrytpted, StandardCharsets.UTF_8));
+
+            Cipher cipher2 = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher2.init(Cipher.DECRYPT_MODE, privateKey);
+            cipher2.update(encrytpted);
+            byte[] decrypted = cipher2.doFinal();
+            System.out.println(new String(decrypted, StandardCharsets.UTF_8));
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static int[] getPrimeNumbersUntil(int maximum) {
@@ -8,23 +43,32 @@ public class Main {
             throw new IllegalArgumentException("Numbers below 2 are not allowed");
         }
 
-        if(maximum == 2){
-            return new int[] {2};
-        }
-
-        boolean[] primeNumbers = new boolean[maximum-2];
+        int[] sieve = new int[maximum-1];
         // index 0 represents prime number 2
-        // false -> primeNumber
-        for(int i = 0; i < primeNumbers.length; i++) {
+        // 0 -> primeNumber
+        int primeNumberCounter = 0;
+        for(int i = 0; i < sieve.length; i++) {
             int number = i+2;
-            if(!primeNumbers[i]) {
-                // j start at next position, because i is the prime number and should therefore be not marked
-                for (int j = i+number; j < primeNumbers.length; j+=number) {
-                    primeNumbers[j] = true;
+            if(sieve[i] == 0) {
+                // j start at next position, because i is the prime number and shall
+                //
+                // therefore not be marked
+                for (int j = i+number; j < sieve.length; j+=number) {
+                    sieve[j] = 1;
                 }
+                primeNumberCounter++;
             }
         }
 
-        return null;
+        int[] res = new int[primeNumberCounter];
+        int primeNumberIndex = 0;
+        for(int i = 0; i < sieve.length; i++){
+            if(sieve[i] == 0) {
+                res[primeNumberIndex] = i+2;
+                primeNumberIndex++;
+            }
+        }
+
+        return res;
     }
 }
